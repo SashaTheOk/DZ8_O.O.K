2fa454e8e1eae243626e2e7856354ab62ca7bc48
SashaTheOk <o.kovalenko.koo@gmail.com>
2023-09-03 00:34:55 +0200

    2

--- main.py
+++ main.py
@@ -21,19 +21,19 @@ def get_birthdays_per_week(users):
     }
     # Проходимося по списку користувачів і перевіряємо їх дні народження
     for user in users:
-        print(user["name"])
+        # print(user["name"])
         birthday = user["birthday"]
         # Отримуємо дату народження для поточного року
         birthday_in_current_year = birthday.replace(year=current_year)  # date(current_year, birthday.month, birthday.day)
         birthday_in_next_year = birthday.replace(year=next_year)
-        print(birthday_in_current_year)
+        # print(birthday_in_current_year)
         # print(birthday_in_next_year)
-        print(current_date)
-        print(week_later_date)
+        # print(current_date)
+        # print(week_later_date)
         # Перевіряємо, чи день народження потрапляє в межі наступного тижня
         if current_date <= birthday_in_current_year <= week_later_date or current_date <= birthday_in_next_year <= week_later_date:
             day_of_week = birthday_in_current_year.strftime('%A')  # Отримуємо назву дня тижня
-            print({day_of_week})
+            # print({day_of_week})
             birthday_dict[day_of_week].append(user["name"])
         
 
@@ -49,7 +49,7 @@ def get_birthdays_per_week(users):
                 birthday_dict['Monday'].extend(birthday_dict[day])
                 birthday_dict[day] = []
     birthday_dict = {day: names for day, names in birthday_dict.items() if names}
-    print(birthday_dict)
+    # print(birthday_dict)
     return birthday_dict
 
 if __name__ == "__main__":
